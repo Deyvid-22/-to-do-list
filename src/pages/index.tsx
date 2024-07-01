452fa20269269995f73a553a6f0694e9ac24c725
Deyvid silva <silvadeyvid017@gmail.com>
2024-07-01 13:16:02 -0300

    finalizando o projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,85 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
-// 'flex justify-between items-center  bg-slate-300 h-[100px]
-// const inter = Inter({ subsets: ["latin"] });
+import { useState, useEffect, useRef} from "react";
+import { FormEvent } from "react";
 
+import { api } from "@/service/api";
 import { FiTrash } from "react-icons/fi"
 
-
+interface CustomerProps {
+   id: string;
+   name:string;
+   email: string;
+   status: boolean;
+   created_at: string;
+}
 
 
 export default function App() {
+
+  const [customer, setCustomer] = useState<CustomerProps[]>([]);
   
+  const nameRef = useRef<HTMLInputElement | null >(null)
+  const emailRef = useRef<HTMLInputElement | null >(null)
+
+  useEffect(()=>{
+     load();
+  },[])
+  
+  async function load() {
+    try {
+      const response = await api.get("/customer");
+      console.log(response.data)
+       setCustomer(response.data)
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  }
+
+  async function handleSubmit(event:FormEvent){
+     event.preventDefault();
+
+     if(!nameRef.current?.value || !emailRef.current?.value) return;
+
+     try{
+     const response = await api.post("/customer",{
+      name:emailRef.current?.value,
+      email:emailRef.current?.value
+    })
+      setCustomer(allCustomers => [...allCustomers,response.data])
+    }catch{}   
+   
+
+    nameRef.current.value = ""
+    emailRef.current.value = ""
+  }
+  async function handleDelete(id:string){
+       try{
+         await api.delete("/customer",{
+          params:{
+            id:id,
+          }
+         })
+
+         const allCustomer = customer.filter((customer)=> customer.id !== id)
+
+         setCustomer(allCustomer)
+
+       }catch(err){
+          console.log(err);
+       }
+  }
+
   return (
    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
     <main className="my-10 w-full md:max-w-2xl">
        <h1 className=" text-3xl font-medium text-white">Cliente</h1>
 
-      <form className="flex flex-col my-6">
+      <form className="flex flex-col my-6" onSubmit={handleSubmit}>
         <label className="font-medium text-white">Nome:</label>
         <input
         type="text"
         placeholder="Digite o seu nome completo"
         className="w-full mb-5 py-1 rounded"
+        ref={nameRef}
         />
       
         <label className=" font-medium text-white">Email:</label>
@@ -28,6 +87,7 @@ export default function App() {
         type="text"
         placeholder="Digite o seu Email completo"
         className="w-full mb-5 py-1 rounded"
+        ref={emailRef}
         />
 
       <input
@@ -38,20 +98,26 @@ export default function App() {
       </form>
 
      <section className="flex flex-col">
-      <article 
-      className="w-full bg-white rounded p-2 relative hover:scale-105 duration-100"
-      >
-        <p>Nome:<span> Deyvid</span></p>
-        <p>Email:<span> Deyvid@gmail.com</span></p>
-        <p>Status<span> ATIVO</span></p>
 
-      <button
-      className=" bg-red-500 w-9 h-9 flex justify-center items-center rounded-lg absolute top-[-4px] right-[-4px]"
-      >
-        <FiTrash size={30} color="#fff"/>
-      </button>
+       {customer.map((params)=>(
 
-      </article>
+       <article 
+         className="w-full bg-white rounded p-2 relative hover:scale-105 duration-100 mt-3"
+         key={params.id}>
+           <p>Nome:<span>{params.name}</span></p>
+           <p>Email:<span>{params.email}</span></p>
+           <p>Status<span>{params.status ? "Ativo" :"Inativo"}</span></p>
+   
+         <button
+         className=" bg-red-500 w-9 h-9 flex justify-center items-center rounded-lg absolute top-[-4px] right-[-4px]"
+         onClick={()=> handleDelete(params.id)}
+         >
+           <FiTrash size={30} color="#fff"/>
+         </button>
+   
+         </article>
+       ))}
+        
      </section>
 
     </main>
